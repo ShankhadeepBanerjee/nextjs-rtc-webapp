441b70c181bf1851c0d55adc197359209326a3cc
Shankhadeep <shankhadeep17.sb@gmail.com>
2023-03-07 20:45:54 +0530

    feat: adds tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,36 @@
 import React from "react";
-
+import Image from "next/image";
 type Props = {};
 
 export default function Index({}: Props) {
-  return <div>Welcome To my Google Meet Clone</div>;
+  return (
+    <div className="">
+      <div></div>
+      <div>
+        <div>
+          <h1>Premium video meetings. Now free for everyone.</h1>
+          <h4>
+            We re-engineered the service we built for secure business meetings,
+            Google Meet, to make it free and available for all.
+          </h4>
+          <span>
+            <button>+ New Meeting</button>
+          </span>
+          <div>
+            <a href="">Learn more, </a>
+            <span>about google meet</span>
+          </div>
+        </div>
+        <div>
+          <Image src={""} alt={""} />
+          <div>Get a link you can share</div>
+          <div>
+            Click <strong>New meeting</strong> to get a link you can send to
+            people you want to meet with
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
 }
