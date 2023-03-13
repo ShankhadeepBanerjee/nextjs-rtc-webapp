e8cdd0bce5df50a66182a8bf0eb28007bef97245
Shankhadeep <shankhadeep17.sb@gmail.com>
2023-03-14 00:06:09 +0530

    feat(UI): adds Custom Button and arranges UI components folder structure

---
+++ lib/client/components/Button/button.tsx
@@ -0,0 +1,33 @@
+import classNames from "classnames";
+import React, { HTMLAttributes } from "react";
+
+type Props = {
+  className?: HTMLAttributes<HTMLButtonElement>;
+  children: JSX.Element | string;
+};
+
+export const Button = ({
+  children,
+  className,
+  ...rest
+}: Props &
+  React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  >) => {
+  return (
+    <button
+      className={classNames(
+        "bg-primary",
+        "text-dark",
+        "p-2",
+        "transition duration-150 ease-in-out",
+        "active:bg-dark active:text-white active:ring-1",
+        className
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,13 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "../lib/client/components";
 type Props = {};
 
 export default function Index({}: Props) {
   return (
-    <div className="h-screen flex flex-col justify-center items-center">
-      <div className="flex gap-x-3">
-        <Link href="/room2#init" className="bg-red-400 p-2">
-          + Create Room
-        </Link>
-      </div>
-      <div></div>
-      <div>
-        <div>
-          <h1>Premium video meetings. Now free for everyone.</h1>
-          <h4>
-            We re-engineered the service we built for secure business meetings,
-            Google Meet, to make it free and available for all.
-          </h4>
-          <span>
-            <button>+ New Meeting</button>
-          </span>
-          <div>
-            <a href="">Learn more, </a>
-            <span>about google meet</span>
-          </div>
-        </div>
-        <div>
-          <Image src={""} alt={""} />
-          <div>Get a link you can share</div>
-          <div>
-            Click <strong>New meeting</strong> to get a link you can send to
-            people you want to meet with
-          </div>
-        </div>
-      </div>
-      <div></div>
+    <div className="flex h-screen flex-col items-center justify-center dark:bg-dark">
+      <Button className="rounded-lg font-semibold ">+ New Meeting</Button>
     </div>
   );
 }
